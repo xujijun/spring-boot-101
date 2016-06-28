cbb5ce872b7db24a887197999c009449bd6da82f
Xu <xujijun@126.com>
2016-06-28 10:51:06 +0800

    build config

--- src/main/java/com/xjj/MySpringBootApplication.java
+++ src/main/java/com/xjj/MySpringBootApplication.java
@@ -1,12 +1,16 @@
 package com.xjj;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringBootApplication {
-
+	private static Logger logger = LoggerFactory.getLogger(MySpringBootApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBootApplication.class, args);
+		logger.info("My Spring Boot Application Started");
 	}
 }
