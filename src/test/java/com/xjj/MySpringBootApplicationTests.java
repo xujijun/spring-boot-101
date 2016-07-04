47d264f23ee966ee95c476534087b637e8e87988
Xu <xujijun@126.com>
2016-07-04 17:48:11 +0800

    junit web test.

--- src/test/java/com/xjj/MySpringBootApplicationTests.java
+++ src/test/java/com/xjj/MySpringBootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.xjj;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +10,18 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ui.velocity.VelocityEngineUtils;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,18 +31,24 @@ import com.xjj.util.XDateUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MySpringBootApplication.class)
+@WebIntegrationTest("server.port:0") //使用随机的port，也可以用这个参数：(randomPort = true)
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MySpringBootApplicationTests {
 	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 	protected final ObjectMapper objectMapper = new ObjectMapper();
 	
 	//@LocalServerPort
 	//private int port;
+	@Value("${local.server.port}")
+    int port;
 	
 	//@Autowired
-	//private TestRestTemplate template;
+	//private TestRestTemplate testRestTemplate;
 	
 	@Autowired
 	PersonDAO personDAO;
+
+	RestTemplate restTemplate = new TestRestTemplate();
 	
 	@Test
 	public void contextLoads() {
@@ -64,5 +80,18 @@ public class MySpringBootApplicationTests {
 		model.put("toUserName", "张三");
 		model.put("fromUserName", "老许");
 		System.out.println(VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "welcome.vm", "UTF-8", model));
+	}
+	
+	@Test
+	public void webTest(){
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+		HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
+		
+		ResponseEntity<String> responseEntity = restTemplate.exchange(
+				"http://localhost:"+port+"/web/hi", HttpMethod.GET,	requestEntity, String.class);
+		System.out.println("port is: " + port);
+		System.out.println(responseEntity.getHeaders());
+		System.out.println(responseEntity.getBody());
 	}
 }
