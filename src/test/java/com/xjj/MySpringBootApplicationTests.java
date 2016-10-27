1eed5314d53763956d5b79038d491a89c876c3af
Xu <xujijun@126.com>
2016-10-27 17:57:41 +0800

    mongoDB config

--- src/test/java/com/xjj/MySpringBootApplicationTests.java
+++ src/test/java/com/xjj/MySpringBootApplicationTests.java
@@ -1,15 +1,24 @@
 package com.xjj;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mongodb.BasicDBObject;
 import com.xjj.config.MyProps;
 import com.xjj.dao.PersonDAO;
 import com.xjj.entity.Person;
@@ -58,6 +67,50 @@ public class MySpringBootApplicationTests extends BasicUtClass{
 		logger.info("person no 2 after update is: {}", objectMapper.writeValueAsString(person2));
 		assertThat(person2.getFirstName(), equalTo("八"));
 
+	}
+
+	@Autowired
+	MongoTemplate mongoTemplate;
+	
+	@Test
+	public void mongoSaveGetTest() throws JsonProcessingException {
+		String dbName = mongoTemplate.getDb().getName();
+		logger.info("db name: {}", dbName);
+		assertThat(dbName, is("test"));
+		
+		for(int i=3;i<=25;i++){
+			Person p = personDAO.getPersonById(i);
+			if(p!=null){
+				mongoTemplate.save(p);
+			}
+		}
+		
+		Criteria c = new Criteria();
+		c.and("id").is(3);
+		Person gotP = mongoTemplate.findOne(Query.query(c), Person.class);
+		logger.debug("p={}", objectMapper.writeValueAsString(gotP));
+		assertThat(gotP.getFirstName(), equalTo("七"));
+		
+		Set<String> collectionNames = mongoTemplate.getDb().getCollectionNames();
+		logger.info("colection names: {}", collectionNames);
+		assertThat(collectionNames, hasItem("person"));
+	}
+	
+	@Test
+	public void mongoAggregationTest() throws JsonProcessingException{
+		Criteria c = new Criteria();
+		c.and("sex").is("F");
+		
+		Aggregation aggr = Aggregation.newAggregation(
+				Aggregation.match(c),
+	            Aggregation.group("lastName").count().as("count")
+	    );
+		AggregationResults<BasicDBObject> aggrResult = mongoTemplate.aggregate(aggr, "person", BasicDBObject.class);
+		if(!aggrResult.getMappedResults().isEmpty()){
+			for(BasicDBObject obj : aggrResult.getMappedResults()){
+				logger.info("count by first name: {}", objectMapper.writeValueAsString(obj));
+			}
+		}	
 	}
 	
 /*	@Autowired
